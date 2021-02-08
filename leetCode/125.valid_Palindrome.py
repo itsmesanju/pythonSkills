4c88a629a59c8efc889d173f6948c7aae6b32e07
itsmesanju <itsmesanju@gmai.com>
2021-02-08 19:48:57 +0000

    LeetCode: Valid palindrome

---
+++ leetCode/125.valid_Palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        for char in s:
+            if not char.isalnum():
+                s = s.replace(char,"").lower()  
+        rev = ''.join(reversed(s))
+        if s.lower() == rev.lower():
+            return True
+        else:
+            return False
