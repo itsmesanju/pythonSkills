511089341b3b44462ca2246ee43bdb0f8d8c30e6
itsmesanju <itsmesanju@gmai.com>
2021-02-18 03:37:00 +0000

    parantheses test

---
+++ leetCode/20.valid_parantheses_stack.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parantheseMap = {'{':'}', '(':')', '[':']'}
+        stack = []
+        for character in s:
+            if character in parantheseMap:
+                stack.append(character)
+            elif not stack or character != parantheseMap[stack.pop()]:
+                return False
+        return not stack
+#Add left parentheses to the stack as they are encountered. 
+#If we encounter a right parentheses, check if the stack is empty (meaning we didn't previously encounter its match) or if the stack's top element is its pair. 
+#At the end return True if the stack is empty
+
+
