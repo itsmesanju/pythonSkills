3a41ffe3fe5da9eda7766b603301964919a3c96b
itsmesanju <itsmesanju@gmai.com>
2021-03-03 22:09:37 +0000

    restructured

--- leetCode/array_find_matching.py
+++ leetCode/array_find_matching.py
@@ -1,10 +1,9 @@
 array_in=[1,2,3,5,61,9,10,22]
-total = 66
+total = 16
 size=len(array_in)
 
 for i in range(size):
     for j in range(i+1, size, 1):
         if array_in[i] + array_in[j] == total:
             print(f'Possible solution is with {array_in[i]} @ index: {i}, {array_in[j]} @ index: {j}')
-    else:
-        print("Null")
+
