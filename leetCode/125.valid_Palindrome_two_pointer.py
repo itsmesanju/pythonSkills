92d123041de514816c8acb908e7188b4d56e190d
itsmesanju <itsmesanju@gmail.com>
2023-12-15 23:38:11 -0500

    Update 125.valid_Palindrome_two_pointer.py

--- leetCode/125.valid_Palindrome_two_pointer.py
+++ leetCode/125.valid_Palindrome_two_pointer.py
@@ -1,39 +1,16 @@
-import re
-class Solution(object):
-    def isPalindrome(self, s):
-        s = re.sub(r'[^a-zA-Z0-9]', '', s).upper()
-        """
-        :type s: str
-        :rtype: bool
-        """
-        '''
-        Idea : use to pointer, one from the begning other from the end (while loop)
-                - check if the letter is valid alphanumeric character 
-                     - if not move to the next one using while loop 
-                - once you get valid letter from both end, compare them 
-                    - if they are not equal 
-                        - return False
-                    - if coninue 
-                - if the loop end before return Flase
-                    - the string is then palindrom 
-                        - So return True 
-        '''
-        if not s:
-            return True
-        i = 0
-        j = len(s)-1
-        print(s)
-        while i <=len(s)-1 and j>=0 and i < j:
-            # check if the letter is valid alphanumeric character
-            while i < len(s)-1 and i < j: 
-                i += 1
-            #  check if the letter is valid alphanumeric character
-            while j > 0 and j > i:
-                j-=1
-            # Check if they are not equal 
-            if s[i] != s[j]:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        
+        # Initialize pointers
+        left, right = 0, len(s) - 1
+        
+        # Two-pointer traversal
+        while left < right:
+            if s[left] != s[right]:
                 return False
-            
-            i += 1
-            j -= 1
+            left += 1
+            right -= 1
+
         return True
