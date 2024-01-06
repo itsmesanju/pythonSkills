537b9d69e6023f2ce5c2aa7790996da4c5f9827b
itsmesanju <itsmesanju@gmail.com>
2024-01-06 14:55:49 -0500

    Create Palindrome ==> 647. palindromic-substrings.py

---
+++ leetCode/Palindrome ==> 647. palindromic-substrings.py
@@ -0,0 +1,17 @@
+#Brute force method to Create list of possible strings and verify if they are palindrome.
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+
+            def is_pal(string):
+                    return string == string[::-1]
+                
+            substrings = []
+            for i in range(len(s)):
+                for j in range(i+1, len(s)+1):
+                    substrings.append(s[i:j])
+            ans = 0
+
+            for s in substrings:
+                if is_pal(s):
+                    ans += 1
+            return ans
