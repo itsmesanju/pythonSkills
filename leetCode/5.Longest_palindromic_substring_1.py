fa56ba126045aeeee04f55518fdd7e8eab9bc5e9
itsmesanju <itsmesanju@gmail.com>
2024-01-06 14:44:07 -0500

    Longest_palindromic_substring_1.py

--- leetCode/5.Longest_palindromic_substring_1.py
+++ leetCode/5.Longest_palindromic_substring_1.py
@@ -1,11 +1,34 @@
-def longestPalindrome(s):
-        s_list = ""
-        s_len  = 0
-        for i in range(len(s)):
-            for j in range(i+1,len(s)+1):
-                rev = s[i:j]
-                if s[i:j] == rev[::-1]:
-                    if len(rev) > s_len:
-                        s_list = s[i:j]
-                        s_len = len(rev)
-        return s_list
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # One way to solve this could be to check each substring but it will be worst time complexity
+        #Instead of bruteforce method, we can traverse the list and assume it as center of the palindrome
+        #and try to check on both sides if this is the longest palindrome
+            
+        result = ""
+        resultLength = 0
+
+        for index in range(len(s)):
+
+        #Now, there are two possibilities... what is the string is odd of even characters. Odd can workout assuming one center
+        #So, in some cases, we can assume an additional range... to run the function
+
+            #Same code base to handle the off length string
+            left_ptr, right_ptr = index, index
+            while left_ptr >= 0 and right_ptr < len(s) and s[left_ptr] == s[right_ptr]:
+                if (right_ptr - left_ptr + 1) > resultLength:
+                    result = s[left_ptr:right_ptr+1]
+                    resultLength = right_ptr - left_ptr + 1
+
+                left_ptr = left_ptr - 1
+                right_ptr = right_ptr + 1
+            
+            #Same code base to handle the even length string
+            left_ptr, right_ptr = index, index+1
+            while left_ptr >= 0 and right_ptr < len(s) and s[left_ptr] == s[right_ptr]:
+                if (right_ptr - left_ptr + 1) > resultLength:
+                    result = s[left_ptr:right_ptr+1]
+                    resultLength = right_ptr - left_ptr + 1
+
+                left_ptr = left_ptr - 1
+                right_ptr = right_ptr + 1
+        return result
