8641e2a2df97c70cd5ee4ed8c896b6c23a389735
itsmesanju <itsmesanju@gmail.com>
2024-01-06 16:35:01 -0500

    Update Palindrome ==> 647. palindromic-substrings.py

--- leetCode/Palindrome ==> 647. palindromic-substrings.py
+++ leetCode/Palindrome ==> 647. palindromic-substrings.py
@@ -15,28 +15,4 @@ class Solution:
                     ans += 1
             return ans
 
-#Dynamic programming.... 
-class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-        
-        result = 0
-
-        for i in range(len(s)):
-
-            #for odd ranges
-            left_ptr, right_ptr = i, i
-
-            while left_ptr >= 0 and right_ptr < len(s) and s[left_ptr] == s[right_ptr]:
-                result += 1
-                left_ptr = left_ptr -1
-                right_ptr = right_ptr + 1
-
-            left_ptr, right_ptr = i, i+1
-            while left_ptr >= 0 and right_ptr < len(s) and s[left_ptr] == s[right_ptr]:
-                result += 1
-                left_ptr = left_ptr -1
-                right_ptr = right_ptr + 1
-
-        return result
-
 
