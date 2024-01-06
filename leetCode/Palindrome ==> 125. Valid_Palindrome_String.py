09506c6c47827e2ea621c4ac269916e454e505c6
itsmesanju <itsmesanju@gmail.com>
2024-01-06 14:50:47 -0500

    Update Palindrome ==> 125. Valid_Palindrome_String.py

--- leetCode/Palindrome ==> 125. Valid_Palindrome_String.py
+++ leetCode/Palindrome ==> 125. Valid_Palindrome_String.py
@@ -15,12 +15,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         
         #First make all strings into lower case and only alphanumeric
-        s = ''.join(c.lower() for c in s if c.isalnum())
-        
         # Initialize 2 pointers. left points to the begining and right points to the end.
+        # Next perform two-pointer traversal together and return false if both pointers value is not same.
+        
+        s = ''.join(c.lower() for c in s if c.isalnum())
         left, right = 0, len(s) - 1
         
-        # Next perform two-pointer traversal together and return false if both pointers value is not same.
         while left < right:
             if s[left] != s[right]:
                 return False
@@ -28,3 +28,12 @@ class Solution:
             right -= 1
 
         return True
+
+#Using inbuild regular expressions.
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).upper()
+        if s == s[::-1]:
+            return True
+        return False
