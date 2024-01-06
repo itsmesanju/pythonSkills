cb9f3968a5aeb36f939ce780c8e16ef0ef82c4f0
itsmesanju <itsmesanju@gmail.com>
2024-01-06 14:49:50 -0500

    Delete leetCode/125.valid_Palindrome_optimized_with_re.py

--- leetCode/125.valid_Palindrome_optimized_with_re.py
+++
@@ -1,7 +0,0 @@
-import re
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = re.sub(r'[^a-zA-Z0-9]', '', s).upper()
-        if s == s[::-1]:
-            return True
-        return False
