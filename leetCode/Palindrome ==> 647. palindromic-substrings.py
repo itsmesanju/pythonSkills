e40e3c1c389d5da2e1a484868146b749b8022b3d
itsmesanju <itsmesanju@gmail.com>
2024-01-06 16:34:18 -0500

    Update Palindrome ==> 647. palindromic-substrings.py

--- leetCode/Palindrome ==> 647. palindromic-substrings.py
+++ leetCode/Palindrome ==> 647. palindromic-substrings.py
@@ -1,7 +1,6 @@
 #Brute force method to Create list of possible strings and verify if they are palindrome.
 class Solution:
     def countSubstrings(self, s: str) -> int:
-
             def is_pal(string):
                     return string == string[::-1]
                 
@@ -15,4 +14,29 @@ class Solution:
                 if is_pal(s):
                     ans += 1
             return ans
+
+#Dynamic programming.... 
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        
+        result = 0
+
+        for i in range(len(s)):
+
+            #for odd ranges
+            left_ptr, right_ptr = i, i
+
+            while left_ptr >= 0 and right_ptr < len(s) and s[left_ptr] == s[right_ptr]:
+                result += 1
+                left_ptr = left_ptr -1
+                right_ptr = right_ptr + 1
+
+            left_ptr, right_ptr = i, i+1
+            while left_ptr >= 0 and right_ptr < len(s) and s[left_ptr] == s[right_ptr]:
+                result += 1
+                left_ptr = left_ptr -1
+                right_ptr = right_ptr + 1
+
+        return result
+
 
