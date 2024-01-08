01c012b18cfdff86b95c5b8583f2b04d7e8db5d6
itsmesanju <itsmesanju@gmail.com>
2024-01-07 22:29:16 -0500

    Update 206.Reverse_linked_list.iterative.py

--- leetCode/206.Reverse_linked_list.iterative.py
+++ leetCode/206.Reverse_linked_list.iterative.py
@@ -4,25 +4,27 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        next_node = None
-        prev_node = None
-        cur_node = head
-        while cur_node is not None: 
-			# save the next node in a temp variable
-            next_node = cur_node.next 
-			
-			# since we saved the next node, we can make the connection 
-            cur_node.next = prev_node
-			
-			# save for next iteration
-            prev_node = cur_node 
-			
-			# Assign to the current node from temp variable
-            cur_node = next_node 
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        """
+        The problem seems to involve reversing a linked list. The idea might be to iterate through the list, changing the direction of pointers to reverse the order of nodes.
+	The approach involves traversing the given singly-linked list and reversing the direction of pointers to achieve the reversal of the list. It employs a two-pointer approach where prev and current pointers are utilized to reverse the links between nodes in the list iteratively.
+        """
+        if head == None or head.next == None:
+            return head
+
+        prev= None
+        current = head
+
+        print(f"Starting at Head: {current}")
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+            print(f"Now=> Current: {current}")
+        return prev
         
-		    # At this point, the cur_node will be None, hence return the prev_node
-        return prev_node
 
 runtime O(n)
 space O(1).
