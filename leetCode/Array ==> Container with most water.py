74c72fb93c564b42d6074549aaf2472a6fc79ec9
itsmesanju <itsmesanju@gmail.com>
2024-01-09 22:31:51 -0500

    Update Array ==> Container with most water.py

--- leetCode/Array ==> Container with most water.py
+++ leetCode/Array ==> Container with most water.py
@@ -1,4 +1,4 @@
-#Comment
+#Comment: Worst complexity
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         size=(len(height))
@@ -12,6 +12,29 @@ class Solution:
                 if maxWater < temp:
                     maxWater = temp
         return maxWater
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        size=(len(height))
+        maxWater=0
+        p1 = 0
+        p2 = size-1
+        while (p1 < p2):
+            print(p1,p2,maxWater)
+            length = min(height[p1],height[p2])
+            width = p2 -p1
+            area = length * width
+            maxWater=max(maxWater,area)
+            #print(height[p1],height[p2])
+            if (height[p1] <= height[p2]):
+                #print("p1 increment")
+                p1 += 1
+            else:
+                #print("p2 decrement")
+                p2 -= 1
+        return maxWater
+
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
