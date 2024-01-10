026731231c2029c93c9e2c3061d82d89bb5f58ba
itsmesanju <itsmesanju@gmail.com>
2024-01-09 22:12:55 -0500

    Update Array ==> Product of Array Except Self.py

--- leetCode/Array ==> Product of Array Except Self.py
+++ leetCode/Array ==> Product of Array Except Self.py
@@ -1,6 +1,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+'''This is very simple approach to solve this question. First of all, we are calculating the product of all "non-zero" numbers and based on count of zeros in the given array, the logic will define the o/p.
 
+If there are more than 2 zeros, the resulting array will be all zeros because at any given time, one of the zero is part of the product.
+If there is one zero, all other items will be zero except the 0th item in given array.
+If there are no zeros, we are deviding the product the the item of each index (in a way to determine the product of except).
+
+'''
         total_product = 1
         zero_count = 0
 
