f9de8f4cde19caddba4c8f46bd1560d0d8a8c9a2
itsmesanju <itsmesanju@gmail.com>
2024-01-15 00:46:56 -0500

    Delete leetCode/268.Missing_number.py

--- leetCode/268.Missing_number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        ret=0
-        for items in range(0,len(nums)+1):
-            if items not in nums:
-               return items
