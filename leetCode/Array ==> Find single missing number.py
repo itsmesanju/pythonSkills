f8712de63c695255de70a6b8ed802ee0ea0e2c26
itsmesanju <itsmesanju@gmail.com>
2024-01-15 00:47:33 -0500

    Update Array ==> Find single missing number.py

--- leetCode/Array ==> Find single missing number.py
+++ leetCode/Array ==> Find single missing number.py
@@ -12,3 +12,10 @@ class Solution:
 
     def singleNumber(self, nums: List[int]) -> int:
         return 2*sum(set(nums)) - sum(nums)
+################
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        ret=0
+        for items in range(0,len(nums)+1):
+            if items not in nums:
+               return items
